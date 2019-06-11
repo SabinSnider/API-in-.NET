b494cdb246583de51a51bb3affe10110fa7b9c22
SabinSnider <sabinvsic@gmail.com>
2019-06-11 10:22:17 +0545

    first

---
+++ WebApi1indexing/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WebApi1indexing.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ WebApi1indexing/Controllers/StudentsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using StudentDataAccess;
+
+namespace WebApi1indexing.Controllers
+{
+    public class StudentsController : ApiController
+    {
+        public IEnumerable<Student> Get()
+        {
+            using (StudentDetailsEntities entities = new StudentDetailsEntities())
+            {
+                return entities.Students.ToList();
+            }
+        }
+
+    }
+}
